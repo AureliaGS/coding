d3fa37cb07466982e19a89860b4542d4a0347cc6
Aurelia Sikka <86154629+AureliaGS@users.noreply.github.com>
2023-06-22 21:45:40 +0200

    #5

--- book_2/Snake/newsnake/snake0.js
+++ book_2/Snake/newsnake/snake0.js
@@ -1,7 +1,39 @@
+var jatek = {
+    tabla: [ 
+    "###############",
+    "#             #",
+    "#             #",
+    "#             #",
+    "#     ####    #",
+    "#     ####    #",
+    "#             #",
+    "#             #",
+    "#             #",
+    "###############"
+    ]
+};
+
 var grafika = {
     canvas: document.getElementById("canvas"),
     negyzetMeret: 30,
     tablaMegrajzolas: function() {
-        var ctx
+        var ctx = grafika.canvas.getContext("2d");
+        var aktualisYoffset = 0;
+        jatek.tabla.forEach(function sorEllenorzes(sor) {
+            sor = sor.split("");
+            var aktualisXoffset = 0;
+            sor.forEach(function karakterEllenorzes(karakter){
+                if(karakter == "#") {
+                    ctx.fillStyle = "black";
+                    ctx.fillRect(aktualisXoffset, aktualisYoffset, grafika.negyzetMeret, grafika.negyzetMeret);
+                }
+                aktualisXoffset += grafika.negyzetMeret;
+            });
+            aktualisYoffset += grafika.negyzetMeret;
+        });
     }
-}
+};
+grafika.tablaMegrajzolas();
+
+
+
